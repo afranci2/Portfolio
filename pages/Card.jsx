7f7d15d879f9c314dc1df03bb6eae93629a00813
El Samp <anthony.francisco.aoct@gmail.com>
2023-02-24 10:16:49 -0500

    WREg

--- pages/Card.jsx
+++ pages/Card.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState, useRef, useEffect } from "react";
+import React,{ useState } from "react";
 import Image from "next/image";
 import { AiFillCloseCircle } from "react-icons/ai";
 
